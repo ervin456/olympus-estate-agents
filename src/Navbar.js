a9406ec18ca4ca37f51014f2a0e9ef556acf3843
ervin456 <lazar.ervin456@gmail.com>
2023-09-06 12:44:12 +0300

    name change

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="nav">
       <Link to="/" className="site-title">
-        Site Name
+        Olympus Estate Agents
       </Link>
       <ul>
         <CustomLink to="/pricing">Pricing</CustomLink>
